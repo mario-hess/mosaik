ba43c00354864cdc6e747e428517b33a14307cdc
Mario Hess <office.hess@gmail.com>
2019-11-27 21:53:18 +0100

    Changed port

--- app.js
+++ app.js
@@ -124,6 +124,6 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
-    app.listen(3000);
+    app.listen(5000);
   })
   .catch(err => console.log(err));
--- controllers/auth.js
+++ controllers/auth.js
@@ -201,7 +201,7 @@ exports.postReset = (req, res, next) => {
             subject: "Password Reset",
             html: `
                 <p>You requested a password reset</p>
-                <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>
+                <p>Click this <a href="http://localhost:5000/reset/${token}">link</a> to set a new password</p>
                 `
           });
         }
